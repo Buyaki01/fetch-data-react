b8ef8188d01620cc3fef48ca42b68dacc4b11de8
Buyaki <rittahbuyaki@gmail.com>
2023-03-01 12:14:56 +0300

    Add Users component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { useState } from "react";
+import Users from "./Users";
+
 function App() {
+  const USERS_URL = 'http://jsonplaceholder.typicode.com/users'
+  const [users, setUsers] = useState([])
+
   return (
     <div className="App">
-      <header>
-      </header>
+      <Users
+        USERS_URL={USERS_URL}
+        users={users}
+        setUsers={setUsers}
+      />
     </div>
   );
 }
